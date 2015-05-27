df60271cf0c5745678093b58aa5c565bf0e00165
Gilles Monnier <gilles_monnier@yahoo.fr>
2015-05-27 13:15:13 -0700

    bdd integration

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  email      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class User < ActiveRecord::Base
+
+	validates :name, :email,  :presence => true
+	
+end
